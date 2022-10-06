9edc276a0d97aa458742636f0775252f672bf324
OdidaProtas <bryodiiidah@gmail.com>
2022-10-06 21:46:02 +0300

    setup db

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { Room } from "./entity/Rooms"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
+    host: process.env.DREAMER_CODES_AUTH_DATABASE_HOST,
+    port: Number(process.env.DREAMER_CODES_AUTH_DATABASE_PORT),
+    username: process.env.DREAMER_CODES_DATABASE_USERNAME,
+    password: process.env.DREAMER_CODES_DATABASE_PASSWORD,
+    database: process.env.DREAMER_CODES_DATABASE,
+    synchronize: false,
     logging: false,
-    entities: [Room],
-    migrations: [],
+    entities: [__dirname + "/entity/**/*{.ts,.js}"],
+    migrations: [__dirname + "/migration/**/*.ts"],
     subscribers: [],
 })
