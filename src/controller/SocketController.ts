e133d5dace56b3dcb910d98844a802c4040c4fe0
OdidaProtas <bryodiiidah@gmail.com>
2022-10-15 08:57:55 +0300

    debug

--- src/controller/SocketController.ts
+++ src/controller/SocketController.ts
@@ -1,9 +1,16 @@
-export function handleSocketConnection(socket:any) {
-    console.log(socket.id)
-    console.log(socket.rooms); // Set { <socket.id> }
-    socket.join("room1");
-    console.log(socket.rooms);
-    if(socket.user){
-        console.log(socket.user)
-    }
+export function handleSocketConnection(socket: any) {
+  console.log(
+    "snmkdkbffhjj \n\n\n\n\n\n\n\n\n\\n\n\n\nnn\n\n\\\n\n\nn\n\n\n\n"
+  );
+  socket.emit("ping", "hehe");
+  socket.on("pong", (msg) => {
+    console.log(msg);
+  });
+  console.log(socket.id);
+  console.log(socket.rooms); // Set { <socket.id> }
+  socket.join("room1");
+  console.log(socket.rooms);
+  if (socket.user) {
+    console.log(socket.user);
+  }
 }
